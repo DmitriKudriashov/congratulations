d3cd51007a24a34f3ca8349c949a4ccf2eaa8426
DmitriKudriashov <kds.120731@gmail.com>
2019-04-20 19:33:56 +0300

    change migration CreateEmails

--- db/migrate/20190419175756_create_emails.rb
+++ db/migrate/20190419175756_create_emails.rb
@@ -2,7 +2,7 @@ class CreateEmails < ActiveRecord::Migration[5.2]
   def change
     create_table :emails do |t|
       t.string :address, default: "", null: false
-      t.string :file_image, default: "", null: false
+      t.references :company, foreign_key: true, null: false
       t.timestamps
     end
   end
