b0f875368773acb725dda1ec958def1f5cb00f8b
DmitriKudriashov <kds.120731@gmail.com>
2019-04-20 19:01:53 +0300

    model email create migration and change Cong-on.jpg

--- db/migrate/20190419175756_create_emails.rb
+++ db/migrate/20190419175756_create_emails.rb
@@ -1,8 +1,8 @@
 class CreateEmails < ActiveRecord::Migration[5.2]
   def change
     create_table :emails do |t|
-      t.string :address
-
+      t.string :address, default: "", null: false
+      t.string :file_image, default: "", null: false
       t.timestamps
     end
   end
