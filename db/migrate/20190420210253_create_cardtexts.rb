372d5a4fc470609786a7b48f5b7ecc467ac1844b
DmitriKudriashov <kds.120731@gmail.com>
2019-04-21 22:12:41 +0300

    before run db:migrate for postcard cardtext

--- db/migrate/20190420210253_create_cardtexts.rb
+++ db/migrate/20190420210253_create_cardtexts.rb
@@ -1,7 +1,8 @@
 class CreateCardtexts < ActiveRecord::Migration[5.2]
   def change
     create_table :cardtexts do |t|
-
+      t.string :filename, default: ''
+      t.text :text, default: ''
       t.timestamps
     end
   end
--- db/migrate/20190420210536_create_email_cards.rb
+++ db/migrate/20190420210536_create_email_cards.rb
@@ -1,8 +1,12 @@
 class CreateEmailCards < ActiveRecord::Migration[5.2]
   def change
     create_table :email_cards do |t|
-
+      t.references :email, foreign_key: true, null: false
+      t.references :postcard, foreign_key: true, null: false
+      t.index [:email_id, :postcard_id], unique: true
       t.timestamps
     end
   end
+
+
 end
