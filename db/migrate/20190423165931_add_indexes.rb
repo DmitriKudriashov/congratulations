fca2e6583d40b2365086ec235a05efd608da2116
DmitriKudriashov <kds.120731@gmail.com>
2019-04-23 22:53:55 +0300

    add new indexes

---
+++ db/migrate/20190423165931_add_indexes.rb
@@ -0,0 +1,9 @@
+class AddIndexes < ActiveRecord::Migration[5.2]
+  def change
+    add_index :cardtexts, :filename, unique: true
+    add_index :countries_holidays, [:country_id, :holiday_id], unique: true
+    add_index :companies_holidays, [:company_id, :holiday_id], unique: true
+    remove_column :emails, :company_id
+    remove_index :emails, name: :index_emails_on_company_id
+  end
+end
