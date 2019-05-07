7986aa516d9556042d040bc857cf380a4d446457
DmitriKudriashov <kds.120731@gmail.com>
2019-05-07 13:20:16 +0300

    add controllers companies,countries, people

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'dates_holidays#index'
+  root 'people#index'
 
   resources :types
+  resources :people
   resources :holidays
   resources :dates_holidays
 
