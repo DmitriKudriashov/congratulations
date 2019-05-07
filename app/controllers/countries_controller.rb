73c9750c65ca77f0ecf46d21f14b4ac5a6fc5e30
DmitriKudriashov <kds.120731@gmail.com>
2019-05-07 16:19:33 +0300

    fill countries_controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,60 @@
 class CountriesController < ApplicationController
+  before_action :set_countries, only: %i[index]
+  before_action :find_country, only: %i[show edit update destroy]
+
+  def index;  end
+
+  def new
+    @country = Country.new
+  end
+
+  def edit; end
+
+  def update
+    if @country.update(country_params)
+      redirect_to country_path(@country)
+    else
+      render :edit
+    end
+  end
+
+
+  def show; end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      redirect_to country_path(@country), notice: 'Success!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @country.destroy
+    redirect_to countries_path, notice: 'Destroy !'
+  end
+
+  def search
+    result = ["Class: #{params.class}", "Parameters: #{params.inspect}"]
+    render plain: result.join("\n")
+  end
+
+  private
+
+  def set_countries
+    @countries = Country.all
+  end
+
+  def find_country
+    @country = Country.find(params[:id])
+  end
+
+  def country_params
+    params.require(:country).permit(:name, :code)
+  end
+
+  def rescue_with_country_not_found
+    render plain: 'Country was not found!'
+  end
 end
