a805a587324a235d52e5a0e69ad721a8b7502da3
DmitriKudriashov <kds.120731@gmail.com>
2019-05-07 15:55:31 +0300

    views companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'people#index'
+  root 'companies#index'
 
+  resources :companies
   resources :types
   resources :people
   resources :holidays
