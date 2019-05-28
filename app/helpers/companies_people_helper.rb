fc7fa0b0e3a427e23e4010e3d81130a6d1b67229
DmitriKudriashov <kds.120731@gmail.com>
2019-05-28 09:28:53 +0300

    fixed errors

--- app/helpers/companies_people_helper.rb
+++ app/helpers/companies_people_helper.rb
@@ -2,8 +2,10 @@ module CompaniesPeopleHelper
 
   def list_companies_people(companies_people)
     c_p = []
-    companies_people.each {|x|  c_p << [x.id, x.person.name + '; '+ x.company.name ] }
+    companies_people.each {|x|  c_p << [x.id, person_company_name(x) ] }
     c_p
   end
-
+  def person_company_name(x)
+    "#{x.person.name} / #{x.company.name}"
+  end
 end
