d44aba3c280cc2f66f685277f522ceccbea97098
DmitriKudriashov <kds.120731@gmail.com>
2019-09-11 12:53:34 +0300

    Repair mailer

--- app/mailers/greetings_mailer.rb
+++ app/mailers/greetings_mailer.rb
@@ -11,23 +11,22 @@ class GreetingsMailer < ApplicationMailer
 
     @greetings_text = email.message
     @from = %("STAFF CENTRE" < #{user.email} >)
-    # @from = %("STAFF CENTRE" <office@staff-centre.com>)
-
     # attachments = list_attachments(email) # почему-то не сработало ???
-
+    @files = []
     email.postcards.each do |postcard|
       file_with_picture = postcard.image.path
       if File.exist?(file_with_picture)
-        attachments[postcard.filename] = File.read(file_with_picture)
+        attachments.inline[postcard.filename] = File.read(file_with_picture)
+        @files << postcard.filename
       end
     end
 
-    mail from: @from, to: @address, subject: 'CONGRATULATIONS !!!'
+    mail from: @from, to: @address, subject: 'CONGRATULATIONS !!!', content_type: "text/html"
     email.sent_date = Time.now
     email.address = @address
     email.save
-  rescue StandardError => e
-    flash[:alert] = " ERROR SAVE EMAIL ! #{e.message} "
+  # rescue StandardError => e
+  #   flash[:alert] = " ERROR SAVE EMAIL ! #{e.message} "
   end
 
   def personal_address(email)
@@ -48,23 +47,5 @@ class GreetingsMailer < ApplicationMailer
       end
     end
     atts
-  end
-
-  def send_message_old(email)
-    @name = email.name
-    @address = email.address
-    @greetings_text = email.greetings_text
-    @from = %("STAFF CENTRE" <office@staff-centre.com>)
-    @postcard = email.postcards.first
-    return unless @postcard.present?
-
-    # @att_file = Rails.root.join("./public#{@postcard.image.url}")
-    # <Pathname:
-    # /Users/Admin/Sites/ROR/congratulations/public/system/postcards/images/000/000/003/original/Rob-Gonsalves-Master-of-Illusion-optical.jpg?1559820537>
-    @att_file = @postcard.image.path
-    if File.exist?(@att_file)
-      attachments[@postcard.filename] = File.read(@att_file)
-      mail from: @from, to: @address, subject: 'CONGRATULATIONS !!!'
-    end
   end
 end
